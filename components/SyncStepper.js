83adc3ef07eb0791f133be6c5e3445fcf9af63c3
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2023-11-03 14:36:36 +0200

    update steppers styles

--- components/SyncStepper.js
+++ components/SyncStepper.js
@@ -5,24 +5,20 @@ import {
   useSendTransaction,
   useWaitForTransaction,
 } from "wagmi";
-// import classNames from "classnames";
-import { Oval } from "react-loader-spinner";
 import Image from "next/image";
-import styles from "../styles/Home.module.scss";
+import { Oval } from "react-loader-spinner";
 
 import { networksConstants } from "../utils/constants";
-import CustomConnectButton from "../components/CustomConnectButton";
 import { requestSyncData } from "../utils/api";
 import { getShortAddress } from "../utils/tools";
+import CustomConnectButton from "../components/CustomConnectButton";
 import logoCheckMark from "../public/images/check-mark-icon.svg";
+import styles from "../styles/Home.module.scss";
+import stylesAddress from "../styles/Address.module.scss";
 
 class CustomError extends Error {}
 
-// const screenWidth = window.screen.width;
-const screenWidth = 600;
-
 function SyncStepper({ syncRequestData, afterSyncAction }) {
-  // eslint-disable-next-line
   const [taskStatus, setTaskStatus] = useState(null);
   const [activeStep, setActiveStep] = useState(0);
   const [error, setError] = useState();
@@ -32,14 +28,12 @@ function SyncStepper({ syncRequestData, afterSyncAction }) {
   const { isConnecting } = useAccount();
   const { chain } = useNetwork();
 
-  console.log(syncRequestData);
-  console.log("activeStep", activeStep);
-
   const { data, status, sendTransaction } = useSendTransaction({
     chainId: syncRequestData.chainId,
     onError(error) {
       console.log("useSendTransaction error:", error);
       setError(error);
+      setActiveStep(2);
     },
     onSuccess(data) {
       setTransactionHash(data.hash);
@@ -51,8 +45,10 @@ function SyncStepper({ syncRequestData, afterSyncAction }) {
     onError(error) {
       console.log("useWaitForTransaction error:", error);
       setError(error);
+      setActiveStep(2);
     },
     onSuccess() {
+      setActiveStep(4);
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
       setTimeout(() => afterSyncAction(), 3000);
       setTransactionHash(data.hash);
@@ -105,20 +101,11 @@ function SyncStepper({ syncRequestData, afterSyncAction }) {
     })();
   }, [syncRequestData]);
 
-  const tickClasses = (step) => "";
-  //   const tickClasses = (step) =>
-  //     classNames("mint-tick-box", activeStep + 1 >= step && "mint-tick-active");
-
-  const messageClasses = "";
-  //   const messageClasses = classNames(
-  //     "mint-message",
-  //     !!error && "mint-message-error",
-  //     status === "loading" && "mint-message-loading",
-  //     (isLoading || taskStatus === "loading") &&
-  //       taskStatus !== "success" &&
-  //       "mint-message-loading",
-  //     (isSuccess || taskStatus === "success") && "mint-message-success"
-  //   );
+  useEffect(() => {
+    if (status === "loading") {
+      setActiveStep(3);
+    }
+  }, [status]);
 
   const getErrorMessage = useCallback(() => {
     if (error instanceof CustomError) {
@@ -133,144 +120,185 @@ function SyncStepper({ syncRequestData, afterSyncAction }) {
 
   const renderMessageBlock = useCallback(() => {
     return (
-      <div>
-        {!!error && getErrorMessage()}
-        {status === "loading" && "Please, confirm to sync"}
-        {(isLoading || taskStatus === "loading") &&
-          taskStatus !== "success" &&
-          "Transaction is pending"}
-        {(isSuccess || taskStatus === "success") && "Success! Activity synced."}
+      <div className={stylesAddress.stepper_message}>
+        <div
+          className={
+            !!error
+              ? stylesAddress.stepper_message_error
+              : status === "loading" ||
+                ((isLoading || taskStatus === "loading") &&
+                  taskStatus !== "success")
+                ? stylesAddress.stepper_message_loading
+                : isSuccess || taskStatus === "success"
+                  ? stylesAddress.stepper_message_success
+                  : {}
+          }
+        >
+          {!!error && getErrorMessage()}
+          {(isLoading || taskStatus === "loading") &&
+            taskStatus !== "success" &&
+            "Transaction is pending"}
+          {(isSuccess || taskStatus === "success") &&
+            "Success!"}
+        </div>
       </div>
     );
   }, [error, status, taskStatus, isSuccess, isLoading, getErrorMessage]);
 
   const renderSyncButton = () => {
     return (
-      <div className="mint-steps-frame mint-steps-button mint-steps-header-button">
-        <button
-          onClick={() => {
-            setError(null);
-            sendTransaction?.({
-              ...syncData,
-            });
-          }}
-          className={styles.button}
-          disabled={
-            activeStep === 0 ||
-            status === "loading" ||
-            status === "success" ||
-            !syncData
-          }
-        >
-          {syncData ? (
-            "Sync activity"
-          ) : (
-            <Oval
-              height={18}
-              width={18}
-              color="#000"
-              wrapperStyle={{ marginLeft: "auto", marginRight: "auto" }}
-              wrapperClass={styles.flex}
-              visible={true}
-              ariaLabel="oval-loading"
-              secondaryColor="rgb(35, 35, 35)"
-              strokeWidth={3}
-              strokeWidthSecondary={5}
-            />
-          )}
-        </button>
-      </div>
+      <button
+        onClick={() => {
+          setError(null);
+          setActiveStep(2);
+          sendTransaction?.({
+            ...syncData,
+          });
+        }}
+        className={styles.button}
+        style={{ width: "160px", marginTop: "16px" }}
+        disabled={
+          activeStep === 0 ||
+          status === "loading" ||
+          status === "success" ||
+          !syncData
+        }
+      >
+        {syncData ? (
+          "Attestation activity"
+        ) : (
+          <Oval
+            height={18}
+            width={18}
+            color="#000"
+            wrapperStyle={{ marginLeft: "auto", marginRight: "auto" }}
+            wrapperClass={styles.flex}
+            visible={true}
+            ariaLabel="oval-loading"
+            secondaryColor="rgb(35, 35, 35)"
+            strokeWidth={3}
+            strokeWidthSecondary={5}
+          />
+        )}
+      </button>
     );
   };
   const renderHashLink = () => {
     return (
-      <>
-        {transactionHash && (
-          <div className="mint-steps-frame mint-steps-button mint-steps-no-border">
-            <a
-              href={`${
-                networksConstants[chain?.id].explorerURL
-              }tx/${transactionHash}`}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="mint-link"
-            >
-              <span>View in explorer&nbsp;</span>
-            </a>
-          </div>
-        )}
-      </>
+      <div className={stylesAddress.stepper_link}>
+        <a
+          href={`${networksConstants[chain?.id | 5000].explorerURL
+            }tx/${transactionHash}`}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="mint-link"
+        >
+          <span>View in explorer&nbsp;</span>
+        </a>
+      </div>
     );
   };
 
   return (
     <div>
-      <div className="mint-steps-block">
-        <div className="mint-steps-header">
-          <div className={tickClasses(1)}>
-            {activeStep >= 1 ? (
-              <Image src={logoCheckMark} alt="check mark" />
-            ) : (
-              <span className="mint-steps-number">1</span>
-            )}
-          </div>
-          <div className="mint-steps-frame mint-steps-input">
-            {activeStep === 0 ? (
-              <CustomConnectButton
-              // className={"mint-button"}
-              // chainId={syncRequestData.chainId}
-              />
-            ) : (
-              <span>
-                {screenWidth < 768
-                  ? getShortAddress(syncRequestData.address, 6, 30)
-                  : syncRequestData.address}
-              </span>
-            )}
-          </div>
+      {activeStep === 4 ? (
+        <div className={styles.flex} style={{gap: "12px", color: "#00872E"}}>
+          <Image src={logoCheckMark} alt="check mark" />
+          <h2>Success!</h2>
         </div>
-        <div className="mint-steps-body">
-          <div className="mint-steps-line"></div>
-        </div>
-
-        <div className="mint-steps-header">
-          <div className={tickClasses(2)}>
-            {activeStep > 1 ? (
-              <Image src={logoCheckMark} alt="check mark" />
-            ) : (
-              <span className="mint-steps-number">2</span>
-            )}
-          </div>
-          {renderSyncButton()}
-        </div>
-
-        <div className="mint-steps-body">
-          <div className="mint-steps-line mint-invisible-line"></div>
-          <div className="mint-steps-content">{renderHashLink()}</div>
-        </div>
-
-        <div className="mint-message-block">
-          <div className={messageClasses}>
-            {(isLoading || taskStatus === "loading") &&
-              taskStatus !== "success" &&
-              !error && (
-                <Oval
-                  height={30}
-                  width={30}
-                  color="#4fa94d"
-                  wrapperStyle={{}}
-                  wrapperClass="mint-spinner"
-                  visible={true}
-                  ariaLabel="oval-loading"
-                  secondaryColor="#4fa94d"
-                  strokeWidth={4}
-                  strokeWidthSecondary={5}
-                />
+      ) : (
+        <>
+          <div className={styles.flex}>
+            <div
+              className={stylesAddress.stepper_tick}
+              style={
+                activeStep + 1 >= 1 ? { backgroundColor: "#FFFFFF50" } : {}
+              }
+            >
+              {activeStep >= 1 ? (
+                <Image src={logoCheckMark} alt="check mark" />
+              ) : (
+                <span>1</span>
               )}
-            {renderMessageBlock()}
+            </div>
+            <div className={stylesAddress.stepper_body}>
+              <h2>Connect Wallet</h2>
+              <div style={{ marginTop: "16px" }}>
+                {activeStep === 0 ? (
+                  <CustomConnectButton />
+                ) : (
+                  <span>
+                    {getShortAddress(syncRequestData.address, 6, 30)}
+                  </span>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+          <div className={stylesAddress.stepper_line}></div>
+
+          <div className={styles.flex}>
+            <div
+              className={stylesAddress.stepper_tick}
+              style={
+                activeStep + 1 >= 2 ? { backgroundColor: "#FFFFFF50" } : {}
+              }
+            >
+              {activeStep > 2 ? (
+                <Image src={logoCheckMark} alt="check mark" />
+              ) : (
+                <span>2</span>
+              )}
+            </div>
+            <div className={stylesAddress.stepper_body}>
+              <h2 style={activeStep + 1 < 2 ? { opacity: "50%" } : {}}>
+                Get attestation
+              </h2>
+              {renderSyncButton()}
+            </div>
+          </div>
+          <div className={stylesAddress.stepper_line}></div>
+
+          <div className={styles.flex}>
+            <div
+              className={stylesAddress.stepper_tick}
+              style={activeStep >= 3 ? { backgroundColor: "#FFFFFF50" } : {}}
+            >
+              {activeStep > 3 ? (
+                <Image src={logoCheckMark} alt="check mark" />
+              ) : (
+                <span>3</span>
+              )}
+            </div>
+            <div
+              className={stylesAddress.stepper_body}
+              style={{ height: "52px" }}
+            >
+              <h2 style={activeStep < 3 ? { opacity: "50%" } : {}}>
+                Confirm attestation
+              </h2>
+              <div style={{ margin: "6px" }}>
+                {activeStep === 3 && (
+                  <Oval
+                    height={30}
+                    width={30}
+                    color="#4fa94d"
+                    wrapperStyle={{}}
+                    wrapperClass="mint-spinner"
+                    visible={true}
+                    ariaLabel="oval-loading"
+                    secondaryColor="#4fa94d"
+                    strokeWidth={4}
+                    strokeWidthSecondary={5}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          {renderMessageBlock()}
+        </>
+      )}
+      {transactionHash && renderHashLink()}
     </div>
   );
 }
