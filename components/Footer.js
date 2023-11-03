d801c93d462f52bbdbff68e4db23ed428992c015
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2023-11-03 17:51:46 +0200

    fix footers mobile styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,15 @@
 import styles from "../styles/Home.module.scss";
 
-const Header = () => {
+const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>© 2023 Mantle.network.&nbsp;</div>
       <div className={styles.flex}>
-        Made by&nbsp;<a href="https://metasvit.io">Metasvit</a>. All right
-        reserved.
+        Made by&nbsp;<a href="https://metasvit.io">Metasvit</a>.&nbsp;
       </div>
+      <div>All right reserved.</div>
     </footer>
   );
 };
 
-export default Header;
+export default Footer;
