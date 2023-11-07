42bf07c9312ead8a0bf0574e4ba6eb12582d2d97
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2023-11-07 18:28:34 +0200

    update status chart

--- components/StatusChart.js
+++ components/StatusChart.js
@@ -1,25 +1,40 @@
 import { useState, useMemo } from 'react'
-import { PieChart, Pie, Sector, Cell } from 'recharts'
+import { PieChart, Pie, Sector, Cell, Tooltip } from 'recharts'
 
-function generateRandomColorsArray(count) {
-  const colors = [];
-  const characters = "0123456789ABCDEF";
+// function generateRandomColorsArray(count) {
+//   const colors = [];
+//   const characters = "0123456789ABCDEF";
 
-  for (let i = 0; i < count; i++) {
-    let color = "#";
-    for (let j = 0; j < 6; j++) {
-      color += characters[Math.floor(Math.random() * 16)];
-    }
-    colors.push(color);
-  }
+//   for (let i = 0; i < count; i++) {
+//     let color = "#";
+//     for (let j = 0; j < 6; j++) {
+//       color += characters[Math.floor(Math.random() * 16)];
+//     }
+//     colors.push(color);
+//   }
 
-  return colors;
-}
+//   return colors;
+// }
+
+const colors = [
+  "rgb(2, 202, 156)",
+  "rgb(255, 194, 77)",
+  "rgb(232, 131, 74)",
+  "rgb(23, 143, 253)",
+  "rgb(69, 76, 251)",
+  "rgb(155, 77, 255)",
+  "rgb(2, 202, 156)",
+  "rgb(255, 194, 77)",
+  "rgb(232, 131, 74)",
+  "rgb(23, 143, 253)",
+  "rgb(69, 76, 251)",
+    "rgb(155, 77, 255)"
+]; 
 
 export default function StatusChart({ data }) {
     const [activeIndex] = useState(0)
 
-    const colors = useMemo(() => generateRandomColorsArray(data.length+2), [data]);
+    // const colors = useMemo(() => generateRandomColorsArray(data.length+2), [data]);
 
     const renderActiveShape = (props) => {
         const { cx, cy, innerRadius, outerRadius, endAngle, fill } = props
@@ -32,25 +47,30 @@ export default function StatusChart({ data }) {
     }
 
     return (
-        <PieChart width={160} height={160}>
-            <Pie
-                activeIndex={activeIndex}
-                activeShape={renderActiveShape}
-                data={data}
-                cx={75}
-                cy={75}
-                innerRadius={50}
-                startAngle={90}
-                endAngle={720}
-                outerRadius={75}
-                paddingAngle={-3}
-                cornerRadius={0}
-                dataKey="value"
-            >
-                {data.map((entry, index) => (
-                    < Cell fill = {colors[index]} key = { entry.name + entry.value } />
-                ))}
-            </Pie>
-        </PieChart>
-    )
+      <PieChart width={280} height={240}>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          data={data}
+          cx={140}
+          cy={120}
+          innerRadius={50}
+          startAngle={90}
+          endAngle={720}
+          outerRadius={75}
+          paddingAngle={-3}
+          cornerRadius={0}
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell
+              fill={colors[index]}
+              key={entry.name + entry.value}
+              dataKey={entry.name}
+            />
+          ))}
+        </Pie>
+        <Tooltip cursor={false} />
+      </PieChart>
+    );
 }
